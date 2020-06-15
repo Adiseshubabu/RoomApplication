715181703193babc6fbc6a051181da0cdb8b54d9
adiseshubabu kaliyapudi <adiseshu.babu99@gmail.com>
2020-06-15 13:07:43 +0530

    Test

--- RoomApplication.UI.ViewModels/InvestmentViewModel.cs
+++ RoomApplication.UI.ViewModels/InvestmentViewModel.cs
@@ -6,13 +6,14 @@ using System.Threading.Tasks;
 
 namespace RoomApplication.UI.ViewModels
 {
-   public class InvestmentViewModel
+    public class InvestmentViewModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public string InvestmentPurpose { get; set; }
         public double Amount { get; set; }
         public DateTime Date { get; set; }
+        public string testc {get;set;}
 
         public dynamic investment()
         {
